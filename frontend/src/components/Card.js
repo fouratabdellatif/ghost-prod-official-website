52209b9707fe170329e4f5f4eb57c08de2d9d79a
marsit-saif-eddine <seifeddine.marsit@etudiant-ipeiem.utm.tn>
2022-03-21 00:12:59 +0100

    added ; card component
    card.css
    card.js

---
+++ frontend/src/components/Card.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './assets/Card.css'
+
+function card({title,soustitle,imageUrl,body}) {
+  return (
+    <div className='card-container'>
+      
+       <div className="card-content">
+          
+          <div className="card-title">
+            <h3>{title}</h3>
+          </div>
+          <div className="card-stitle">
+            <h>{soustitle}</h>
+          </div>
+          <div className="Card-body">
+            <p>{body}</p>
+          </div>
+         
+
+
+        </div>
+        
+        <div className="image-container">
+            <img src={imageUrl} alt=''/>
+
+
+        </div>
+        
+       
+        
+        
+         
+    </div>
+  )
+}
+
+export default card
