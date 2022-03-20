6acc129116f43e1f8edb9b6218879ea38de4da81
Fourat Abdellatif <fourat-abdellatif99@hotmail.fr>
2022-03-20 16:47:37 +0100

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import Aos from "aos";
+import React, { useEffect, useState } from "react";
+import Dropdown from "./components/Dropdown";
+import Hero from "./components/Hero";
+import InfoSection from "./components/InfoSection";
+import Navbar from "./components/Navbar";
+import { InfoData } from "./data/InfoData";
+import { SliderData } from "./data/SliderData";
+import GlobalStyle from "./globalStyles";
+
+function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, [])
+
+  return (
+    <>
+      <GlobalStyle />
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Hero slides={SliderData} />
+      <InfoSection {...InfoData} />
+    </>
+  );
+}
+
+export default App;
