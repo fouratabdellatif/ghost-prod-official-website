7bd8222cceaa1403e69af4de7e8b95adfd0f45c7
Fourat Abdellatif <fourat-abdellatif99@hotmail.fr>
2022-04-09 17:55:55 +0100

    add loading screen
    it's not final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import Aos from "aos";
 import Dropdown from "./components/Dropdown";
@@ -7,10 +8,18 @@ import GlobalStyle from "./globalStyles";
 import HomePage from "./pages/HomePage";
 import ScrollToTop from "./components/ScrollToTop";
 import ScrollToTopButton from "./components/ScrollToTopButton";
+import LoadingScreen from "./components/LoadingScreen";
 
 function App() {
 
   const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() =>
+      setLoading(!isLoading)
+      , 1500)
+  }, []);
 
   const toggle = () => {
     setIsOpen(!isOpen)
@@ -20,7 +29,16 @@ function App() {
     Aos.init({ duration: 1000 });
   }, [])
 
-  return (
+  return isLoading ? (
+    <LoadingScreen
+      loading={isLoading}
+      bgColor='#292929'
+    // spinnerColor='#000'
+    // textColor='#676767'
+    // logoSrc='../public/images/logo/logo.png'
+    // text='Veuillez patienter pendant le chargement de la page...'
+    />
+  ) : (
     <>
       <GlobalStyle />
       <ScrollToTop />
