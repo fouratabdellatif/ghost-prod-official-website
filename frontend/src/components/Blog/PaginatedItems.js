85df0ededfdbdde2815f8d543911212243845650
Fourat Abdellatif <fourat-abdellatif99@hotmail.fr>
2022-04-17 03:21:15 +0100

    add blog page
    not 100% finished --XXX

---
+++ frontend/src/components/Blog/PaginatedItems.js
@@ -0,0 +1,93 @@
+import React, {
+    useEffect,
+    useState
+} from "react";
+import ReactPaginate from "react-paginate";
+import '../../assets/css/PaginatedItems.css';
+import PostCard from "../Cards/PostCard";
+
+function Items({ currentItems }) {
+    return (
+        <div className="page-items">
+            {currentItems && currentItems.map((item, index) => (
+                <PostCard
+                    key={index}
+                    name={item.text}
+                    category={item.category}
+                    image={item.image}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default function PaginatedItems({ itemsPerPage, items }) {
+    // We start with an empty list of items.
+    const [currentItems, setCurrentItems] = useState(null);
+    const [pageCount, setPageCount] = useState(0);
+    // Here we use item offsets; we could also use page offsets
+    // following the API or data you're working with.
+    const [itemOffset, setItemOffset] = useState(0);
+
+    useEffect(() => {
+        // Fetch items from another resources.
+        const endOffset = itemOffset + itemsPerPage;
+        console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+        setCurrentItems(items.slice(itemOffset, endOffset));
+        setPageCount(Math.ceil(items.length / itemsPerPage));
+    }, [itemOffset, items, itemsPerPage]);
+
+    // Invoke when user click to request another page.
+    const handlePageClick = (event) => {
+        const newOffset = event.selected * itemsPerPage % items.length;
+        console.log(`User requested page number ${event.selected}, which is offset ${newOffset}`);
+        setItemOffset(newOffset);
+    };
+
+    return (
+        <>
+            {/* <ReactPaginate
+                nextLabel=">"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                marginPagesDisplayed={2}
+                pageCount={pageCount}
+                previousLabel="<"
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakLabel="..."
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                containerClassName="pagination"
+                activeClassName="active"
+                renderOnZeroPageCount={null}
+            /> */}
+            <Items currentItems={currentItems} />
+            <ReactPaginate
+                nextLabel=">"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                marginPagesDisplayed={2}
+                pageCount={pageCount}
+                previousLabel="<"
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakLabel="..."
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                containerClassName="pagination"
+                activeClassName="active"
+                renderOnZeroPageCount={null}
+                disabledClassName="disabled"
+            />
+        </>
+    );
+}
