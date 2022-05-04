b75950e477ec676551e55b94131aec7098071d18
Fourat Abdellatif <fourat-abdellatif99@hotmail.fr>
2022-05-04 03:03:22 +0100

    add onhover video to project card

--- frontend/src/components/Cards/ProjectCard.js
+++ frontend/src/components/Cards/ProjectCard.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../../assets/css/ProjectCard.css';
 import { Link } from 'react-router-dom';
+import HoverVideoPlayer from "react-hover-video-player";
 
 function ProjectCard({ item }) {
     return (
         <Link to='/' className='project-card-container' data-aos="slide-up"
-            // style={{
-            //     background: `url(${image})`,
-            //     backgroundPosition: 'fixed',
-            //     backgroundSize: 'cover'
-            // }}
+        // style={{
+        //     background: `url(${image})`,
+        //     backgroundPosition: 'fixed',
+        //     backgroundSize: 'cover'
+        // }}
         >
             <div className="project-card-content">
                 <div className="project-card-category">
@@ -23,7 +24,16 @@ function ProjectCard({ item }) {
                 </div>
             </div>
             <div className="project-image-container">
-                <img src={item.image} alt='' />
+                {/* <img src={item.image} alt='' /> */}
+                <HoverVideoPlayer
+                    className='project-hover-video'
+                    videoSrc={item.video}
+                    videoStyle={{
+                        height: '360px',
+                    }}
+                    pausedOverlay={<img src={item.image} alt='' />}
+                // loadingOverlay={<LoadingOverlay />}
+                />
             </div>
         </Link>
     )
