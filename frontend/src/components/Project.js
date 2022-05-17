1428236993958b7720473903dc932d60f780856d
Fourat Abdellatif <fourat-abdellatif99@hotmail.fr>
2022-05-17 13:41:05 +0100

    update projects

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -11,10 +11,10 @@ const Project = ({ item }) => {
                     <YouTube opts={{
                         width: '100%',
                         height: '100%'
-                    }} className='yt-video' videoId="WjnsXb3K9O8" />
+                    }} className='yt-video' videoId={item.videoId} />
                 </div>
                 <div className='one-project-column-right'>
-                    <h1>{item.name}</h1>
+                    <h1 className='one-project-title'>{item.name}</h1>
                     <CustomDivider fullWidth />
                     <p><span>Client: </span><a className='contact-link' href={item.clientLink} target='_blank' rel="noreferrer">{item.client}</a></p>
                     <p><span>Description: </span><br />{item.description}</p>
@@ -24,6 +24,20 @@ const Project = ({ item }) => {
                         <>
                             <p><a className='contact-link' href={partner.partnerLink}>{partner.name}</a></p>
                         </>
+                    ))}
+                </div>
+            </div>
+            <div className='other-projects-section'>
+                <h1 className='other-projects-title'>Also in this project</h1>
+                <CustomDivider right />
+                <div className='other-projects'>
+                    {item.videos && item.videos.map((vid, index) => (
+                        <div data-aos="fade-up">
+                            <YouTube opts={{
+                                width: '100%',
+                                height: '100%'
+                            }} className='yt-video' videoId={vid.videoId} />
+                        </div>
                     ))}
                 </div>
             </div>
