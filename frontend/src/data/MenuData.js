3f6adb61e5717cc4966ca400ef73841befcefe7a
Fourat Abdellatif <63204800+fouratabdellatif@users.noreply.github.com>
2022-08-11 15:20:40 +0100

    remove admin and frontend

--- frontend/src/data/MenuData.js
+++
@@ -1,9 +0,0 @@
-export const menuData = [
-    { title: 'à propos', link: '/about-us' },
-    { title: 'équipe', link: '/team' },
-    { title: 'Services', link: '/services' },
-    { title: 'Réalisations', link: '/realisations' },
-    { title: 'Voice-Over', link: '/voice-over' },
-    { title: 'Blog', link: '/blog' },
-    { title: 'Contact', link: '/contact' },
-]
--- frontend/src/data/NavbarData.js
+++
@@ -1,7 +0,0 @@
-export const navbarData = [
-    { title: 'accueil', link: '/' },
-    { title: 'à propos', link: '/about-us' },
-    { title: 'Services', link: '/services' },
-    { title: 'Blog', link: '/blog' },
-    { title: 'contact us', link: '/contact' },
-]
