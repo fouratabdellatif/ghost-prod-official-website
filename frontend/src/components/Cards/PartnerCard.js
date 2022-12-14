7e772ce8d5072edc96a5075fc8adbae76f34f73f
Fourat Abdellatif <63204800+fouratabdellatif@users.noreply.github.com>
2022-12-14 16:44:16 +0100

    update partner card
    added tooltip with partner name
    added partner link with scale transform

--- frontend/src/components/Cards/PartnerCard.js
+++ frontend/src/components/Cards/PartnerCard.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import '../../assets/css/PartnerCard.css'
 
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+
 const PartnerCard = ({ item }) => {
     return (
-        <img className='partner-image' src={item?.imageFile} alt={item?.imageFile} />
+        <Tippy content={item.name} className='partner-tooltip'>
+            <a href={item.link} target="_blank" rel="noreferrer">
+                <img className='partner-image' src={item?.imageFile} alt={item?.imageFile} />
+            </a>
+        </Tippy>
     )
 }
 
