522057f2977ffebf44497c0f02894cb12e033f1d
Fourat Abdellatif <63204800+fouratabdellatif@users.noreply.github.com>
2022-12-21 17:48:39 +0100

    Update TextData.js

--- frontend/src/data/TextData.js
+++ frontend/src/data/TextData.js
@@ -2,12 +2,21 @@
 
 export const TextData = [
     {
-        text: 'Ghostprod est une agence de production audiovisuelle créée en mars 2019, située au 07 rue du Marthyr Mefteh Triki Manar 1 dans un building de 500 m².',
+        text: "Ghostprod est une entreprise de production audiovisuelle spécialisée dans la réalisation de films d'entreprises, de documentaires, la couverture évènementielle et la production des émissions digitales.",
     },
     {
-        text: 'Nous disposons de deux espaces conçus pour l’installation de deux plateaux de tournage : Un plateau pour des émissions digitales, corporatif, Packshot produit, Shooting etc. Un plateau pour des émissions de Cooking Show, Packshot publicitaire culinaire etc. 3 Suites de Post-production, 1 Salle de régie vidéo, Live streaming, 1 Salle d’enregistrement son et voix off',
+        text: "Fondée en 2019, Ghostprod s'est rapidement imposée comme l'une des sociétés de production les plus innovantes et créatives de la région.",
     },
     {
-        text: 'Tout le building est doté d’une connexion câblée en Fibre Optique et d’une solution de Backup. Un jardin et un espace de détente pour le confort plateau.',
+        text: "Grâce à une équipe de professionnels expérimentés et passionnés, Ghostprod est en mesure de répondre à tous les besoins en matière de production audiovisuelle. Que vous souhaitiez réaliser un film publicitaire, un documentaire ou une émission digitale, Ghostprod saura vous accompagner tout au long de votre projet pour en garantir la réussite.",
+    },
+    {
+        text: "En plus de sa grande expertise en matière de production audiovisuelle, Ghostprod se distingue par son approche innovante et créative. L'entreprise met en place des stratégies de production originales pour chaque projet, en se basant sur les dernières tendances en matière de storytelling et de création audiovisuelle.",
+    },
+    {
+        text: "Grâce à sa collaboration avec les meilleurs talents de la région, Ghostprod est en mesure de proposer des contenus de qualité supérieure, qui sauront toucher le cœur des spectateurs et des téléspectateurs.",
+    },
+    {
+        text: "Si vous souhaitez en savoir plus sur les services proposés par Ghostprod ou si vous souhaitez discuter de votre projet de production audiovisuelle, n'hésitez pas à contacter l'entreprise pour obtenir plus d'informations. Ghostprod se fera un plaisir de vous accompagner dans la réalisation de votre projet.",
     },
 ]
