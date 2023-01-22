1d717d7b523cb1768d93f10797266dff5edfda31
Fourat Abdellatif <63204800+fouratabdellatif@users.noreply.github.com>
2023-01-22 16:45:23 +0100

    Update NavbarData.js

--- frontend/src/data/NavbarData.js
+++ frontend/src/data/NavbarData.js
@@ -1,10 +1,10 @@
 export const navbarData = [
     { title: 'accueil', link: '/' },
-    { title: 'à propos', link: '/a-propos' },
-    { title: 'équipe', link: '/equipe' },
+    // { title: 'à propos', link: '/a-propos' },
+    // { title: 'équipe', link: '/equipe' },
     { title: 'Services', link: '/services' },
     { title: 'Projets', link: '/projets' },
     { title: 'Blog', link: '/blog' },
-    { title: 'voice-over', link: '/voice-over' },
+    // { title: 'voice-over', link: '/voice-over' },
     { title: 'contact', link: '/contact' },
 ]
